e73822faaf7eef2be0c19a48de4d855402821e4a
github-action-benchmark <github@users.noreply.github.com>
2023-02-16 00:09:50 +0000

    add Example Benchmark (customSmallerIsBetter) benchmark result for 3384b409bbd4ef97a0ce9e102e65830f67709194

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1676506190583,
+  "repoUrl": "https://github.com/scheibo/benchmark-action",
+  "entries": {
+    "Example Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kjs@scheibo.com",
+            "name": "Kirk Scheibelhut",
+            "username": "scheibo"
+          },
+          "committer": {
+            "email": "kjs@scheibo.com",
+            "name": "Kirk Scheibelhut",
+            "username": "scheibo"
+          },
+          "distinct": true,
+          "id": "3384b409bbd4ef97a0ce9e102e65830f67709194",
+          "message": "fix CI",
+          "timestamp": "2023-02-15T16:09:06-08:00",
+          "tree_id": "59e4aa59fef056a3d37a6123de4bb72e96fecb75",
+          "url": "https://github.com/scheibo/benchmark-action/commit/3384b409bbd4ef97a0ce9e102e65830f67709194"
+        },
+        "date": 1676506189811,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 10099,
+            "unit": "ns/call",
+            "range": "±0.00%",
+            "extra": "8900"
+          },
+          {
+            "name": "fib(20)",
+            "value": 128822,
+            "unit": "ns/call",
+            "range": "±0.00%",
+            "extra": "1094600"
+          }
+        ]
+      }
+    ]
+  }
+}
